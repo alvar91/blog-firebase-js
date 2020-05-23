638ff2b673647c441750130a8a7021b904cea2fe
Aleksey <aleksey91scorp@bk.ru>
2020-05-23 23:13:17 +0300

    posts render added

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -10,6 +10,39 @@ export class PostsComponent extends Component {
   async onShow() {
     const fbData = await apiService.fetchPosts();
     const posts = TransformService.fbObjectToArray(fbData);
-    console.log(posts);
+    const html = posts.map((post) => renderPost(post));
+
+    this.$el.insertAdjacentHTML("afterbegin", html.join(" "));
   }
+
+  onHide() {
+    this.$el.innerHTML = "";
+  }
+}
+
+function renderPost(post) {
+  const tag =
+    post.type === "news"
+      ? '<li class="tag tag-blue tag-rounded">Новость</li>'
+      : '<li class="tag tag-rounded">Заметка</li>';
+
+  const button =
+    '<button class="button-round button-small button-primary">Сохранить</button>';
+
+  return `        
+    <div class="panel">
+      <div class="panel-head">
+        <p class="panel-title">${post.title}</p>
+        <ul class="tags">
+          ${tag}
+        </ul>
+      </div>
+      <div class="panel-body">
+        <p class="multi-line">${post.fulltext}</p>
+      </div>
+      <div class="panel-footer w-panel-footer">
+        <small>${post.date}</small>
+        ${button}
+      </div>
+    </div>`;
 }
